c40b5543ad4c6b019d35f5123209f566b7152111
Muslim Sultanov <muslim.sultanov@uchi.ru>
2019-05-20 18:53:38 +0300

    remove token from bot

--- app.rb
+++ app.rb
@@ -1,2 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'telegram/bot'
+
+token = ARGV.first
+puts token
+puts 'start'
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    puts 'message'
+    case message.text
+    when '/start'
+      puts 'starrrrrrrrrrrrrrrrrrrrrrrrt'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
