7fa72c15439541d921f73d5f3b8b20ddac64596f
Muslim Sultanov <muslim.sultanov@uchi.ru>
2019-05-20 19:15:44 +0300

    playing with api

--- app.rb
+++ app.rb
@@ -10,8 +10,11 @@ Telegram::Bot::Client.run(token) do |bot|
     puts 'message'
     case message.text
     when '/start'
-      puts 'starrrrrrrrrrrrrrrrrrrrrrrrt'
+      question = 'Выберите вариант ЕГЭ (1 - 19) ?'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      answers =
+        Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(A B), %w(C D)], one_time_keyboard: true)
+    bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
